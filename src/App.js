7e8336711d06b1f562a575ccdad98535d8658c19
Shrestha Gyanendra <gyanendra@yunoMacBook-puro.local>
2020-05-07 14:00:23 +0545

    first_react_app

--- src/App.js
+++ src/App.js
@@ -1,54 +1,125 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import GoogleMapReact from 'google-map-react';
+//import logo from './logo.svg';
+import './App.css'; 
+import Flat from "./components/flat";
+import Marker from "./components/marker";
 
-import SingleTodo from "./SingleTodo";
-
-class App extends React.Component{
-  constructor(){
-    super();
+//we call API and get the json
+class App extends Component{
+  constructor(props){
+    super(props);
     this.state = {
-      todos : [],
-      currentTodo: ""
+      flats:[],
+      allFlats:[],
+      selectedFlat: null,
+      search: ""
     };
   }
 
-  onInputChange = e => {
-    this.setState({currentTodo: e.target.value});
+  componentDidMount(){
+    const url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json";
+    fetch(url) //AJAX
+    .then(response => response.json())
+    .then((data) =>{
+      //console.log(data);
+      this.setState({
+        flats: data,
+        allFlats: data
+      });
+    })
   }
 
-  onClick = () => {
-    let todosCopy = this.state.todos.slice(); // copy todos to todosCopy
-    todosCopy.push(this.state.currentTodo);
-    this.setState({todos: todosCopy, currentTodo:""});
+  selectFlat = (flat) => {
+    console.log(flat);
+    this.setState({
+      selectedFlat: flat
+    });
   }
 
-  deleteTodo = i => {
-    let todosCopy = this.state.todos.slice();
-
-    todosCopy.splice(i, 1);
-    this.setState({todos: todosCopy})
+  handleSearch = (event) => {
+    console.log(event);
+    this.setState({
+      search: event.target.value,
+      flats: this.state.allFlats.filter((flat) => new RegExp(event.target.value, "i").exec(flat.name))
+    });
   }
 
   render(){
-    let bulletedTodos = this.state.todos.map((e, i) =>{
-      return(
-      // <li key={i}>{e}</li>
-      <SingleTodo todo={e} delete={() => {
-        this.deleteTodo(i)
-      }} />
-      );
-    });
+    let center = {
+      lat: 48.8566,
+      lng: 2.3522
+    }
+
+    if (this.state.selectedFlat)
+    {
+      center = {
+        lat: this.state.selectedFlat.lat,
+        lang: this.state.selectedFlat.lang
+      }
+    }
+    // const flat = {
+    //   "id": 148,
+    //   "name": "Trendy Apt in Buttes Montmartre",
+    //   "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat2.jpg",
+    //   "price": 200,
+    //   "priceCurrency": "EUR",
+    //   "lat": 48.885707,
+    //   "lng": 2.343543
+    //  };
+
+    // const flats = [flat, flat , flat, flat]; //flates changes depending on search or the action we do on map
+
     return(
-      <div>
-        <input placeholder="Enter todo" value={this.state.currentTodo}
-        onChange={this.onInputChange} />
-        <button onClick={this.onClick}>Add!</button>
-        <br />
-    {this.state.todos.length === 0 ? "No todos yet!" : <ul>{bulletedTodos}</ul>}
+      // <div>
+      //   <Flat flat={flat} />
+      //   <Flat flat={flat} />
+      // </div>
+      //Flexbox implemenation
+      <div className="app">
+        <div className="main">
+          <div className="search">
+            <input 
+              type="text" 
+              placeholder="Search.." 
+              value={this.state.serach} 
+              onChange={this.handleSearch}/>
+          </div>
+          <div className="flats">
+             {this.state.flats.map((flat) => {
+               return <Flat 
+               key={flat.name} 
+               flat={flat} 
+               selectFlat={this.selectFlat} />
+             })}
+          </div>
+        </div>
+        <div className="map">
+          <GoogleMapReact
+            center={center}
+            zoom={11}
+          >
+            {this.state.flats.map((flat) => {
+               return <Marker 
+               key={flat.name} 
+               lat={flat.lat} 
+               lng={flat.lng} 
+               text={flat.price}
+               selected={flat === this.state.selectedFlat} />
+             })}
+          </GoogleMapReact>
+        </div>
       </div>
     );
   }
 }
-
+// componentDidMount(){
+//   fetch("https://github.com/lewagon/flats-boilerplate/blob/master/flats.json")
+//   .then(response => response.json())
+//   .then((data) => {
+//     this.setState({
+//       flats: data
+//     })
+//   }
+// }
 export default App;
---
+++ src/components/flat.js
@@ -0,0 +1,38 @@
+import React, {Component} from "react";
+import "./flat.css";
+
+// const flat = {
+//     name: "Great flat",
+//     price: 100,
+//     // etc.
+// }
+
+// <Flat flat={flat} />
+
+class Flat extends Component{
+    handleClick = () => {
+        // Call the parent method selectFlat
+        this.props.selectFlat(this.props.flat);
+    }
+    render(){
+        const title = this.props.flat.price 
+          + this.props.flat.priceCurrency + 
+          " - " + this.props.flat.name;
+
+        const style = {
+            // template literals ES6
+            backgroundImage: `url('${this.props.flat.imageUrl}')`
+        };
+
+        return (
+        <div className="flat" onClick={this.handleClick}>
+            <div className="flat-picture" style={style}></div>
+            <div className="flat-title">
+                {title}
+            </div>
+        </div>
+        );
+    }
+    }
+
+export default Flat;
